498746bbb9f282ba5546ffc1381de1aca0cbb7a2
vincent <vincent@debian.vincent>
2023-05-09 16:56:56 +0300

    bunch of empty files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Tasks } from "./screens/Tasks";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Tasks">
+       <Stack.Screen
+          name="Tasks"
+          component={Tasks}
+        />
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Tasks.ts
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export const Tasks = () => {
+   return (
+     <Text>'Hello from menu'</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+})
