d48bdc2541f6d0ef7c42df28905dd40f6542811a
vincentkengo-ha <98455946+vincentkengo-ha@users.noreply.github.com>
2023-05-11 14:10:44 +0300

    fixed import

--- storage/Storage.ts
+++ storage/Storage.ts
@@ -1,4 +1,4 @@
-import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const saveCountables = async (countables: any) => {
   const jsonCountables = JSON.stringify(countables);
