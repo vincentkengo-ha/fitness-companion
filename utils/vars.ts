9508fca89758487fe5f32eeae9cbc32f2c439c98
vincentkengo-ha <98455946+vincentkengo-ha@users.noreply.github.com>
2023-05-14 23:09:54 +0300

    gps utilities

---
+++ utils/vars.ts
@@ -0,0 +1,8 @@
+export function defaultLocation() {
+  let longitude = 60.10351605424711,
+    latitude = 19.928358848970145;
+  return {
+    'longitude': longitude,
+    'latitude': latitude,
+  };
+}
