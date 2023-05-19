9476264879bd6848ae2757a0ec4b5be7d2cf2457
vincentkengo-ha <98455946+vincentkengo-ha@users.noreply.github.com>
2023-05-19 03:21:30 +0300

    saveChanges

--- screens/EditTask.tsx
+++ screens/EditTask.tsx
@@ -8,6 +8,8 @@ import {
 import { useRoute } from "@react-navigation/native";
 import { useState } from "react";
 
+import { saveCountables } from "../storage/Storage";
+
 // Pass in a task and edit it
 export const EditTask = (props: { navigation: any }) => {
   const route = useRoute();
@@ -15,11 +17,25 @@ export const EditTask = (props: { navigation: any }) => {
   const { countables, index } = route.params;
   const name = countables[index].name;
   const description = countables[index].description;
-  const [newName, setName] = useState("");
-  const [newDescription, setDescription] = useState("");
+  const [newName, setName] = useState(countables[index].name);
+  const [newDescription, setDescription] = useState(
+    countables[index].description
+  );
 
   const saveChanges = (name: string, description: string, index: number) => {
+    const newState = [...countables];
 
+    newState[index].name = name;
+    if (name.length === 0) {
+      newState[index].name = countables[index].name;
+    }
+
+    newState[index].description = description;
+    if (description.length === 0) {
+      newState[index].description = countables[index].description;
+    }
+
+    saveCountables(newState);
   };
 
   return (
@@ -34,7 +50,14 @@ export const EditTask = (props: { navigation: any }) => {
         placeholder={description}
         onChangeText={setDescription}
       />
-
+      <TouchableOpacity
+        onPress={() => (
+          saveChanges(newName, newDescription, index),
+          props.navigation.navigate("Tasks")
+        )}
+      >
+        <Text>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 };
